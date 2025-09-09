f7dbecbbb1f9d13b00e92eb092f9fa0b51c42bd7
Danilo Pietropaolo <francodanilo136@gmail.com>
2025-09-09 16:22:49 -0300

    agrego controllers

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import morgan from "morgan";
+import tareasRoutes from "./router/tareas.routes.js";
+import authRoutes from "./router/auth.routes.js";
 
 const app = express();
 
+//Middlewares
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -10,12 +13,10 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   res.send({ message: "Bienvenidos" });
 });
+app.use("/api", tareasRoutes);
+app.use("/api", authRoutes);
 
-app.get("/test", (req, res) => {
-  throw new Error("Error generado por el usuario");
-  res.send("Test");
-});
-
+//Manejo de errores
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
